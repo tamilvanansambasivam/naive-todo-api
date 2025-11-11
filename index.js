2e2f974da4f14c92a13879b1f46b120d7d8cc53a
tamilvanan <tamilvanansambasivam@gmail.com>
2025-11-11 08:08:23 +0530

    add & get todos

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const app = express();
 
 app.use(express.json()); // Allows JSON body parsing
 
+let todos = [
+  { id: 1, title: "Learn API basics", completed: false },
+  { id: 2, title: "Build ToDo API", completed: true },
+];
+
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
